7fbb11500aee219f919194b961ba27d2707fbcec
Vivekmoyaje <vivekmoyaje@gmail.com>
2025-02-12 10:51:11 +0530

    file added

--- paginated-user-list/vite.config.js
+++ paginated-user-list/vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
+  build: {
+    outDir: "build", // This makes sure Vite outputs to the 'build' folder
+  },
+
+  base: "/machine-coding-tasks", // Add this line
   plugins: [tailwindcss()],
 });
